f2df1ae1a5c9feb5dbb13c6d3c8f7fd4f7234886
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2018-09-06 19:30:30 +0200

    Tried some js

---
+++ main.js
@@ -0,0 +1,6 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    //document.getElementById("demo").innerHTML = x;
+
+    var win = window.open("https://www.youtube.com/results?search_query=hello", "_self")
+}
